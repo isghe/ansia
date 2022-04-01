23c89980ba9dd0e0276659393d5b405f1e4eb17e
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2022-04-01 14:08:08 +0200

    npx xo test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+// es-lint disable capitalized-comments
+// es-lint disable unicorn/no-array-for-each
+document.querySelectorAll('*').forEach($element => {
+	$element.hidden = false;
+});
+document.querySelector('#piano-container').hidden = true;
+
